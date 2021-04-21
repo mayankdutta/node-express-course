762f601a44696d5b12981374ad23f6b3b31b644c
Mayank Sharma <info@Mayanks-MacBook-Air.local>
2021-04-21 19:57:35 +0530

    add first get route

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ const app = express();
 app.listen(8000, function () {
   console.log("Server is running");
 });
+
+const mockUserData = [{ name: "Mayank" }, { name: "Dutta" }];
+app.get("/users", function (req, res) {
+  req.json({
+    success: true,
+    message: "successful !!",
+    users: mockUserData,
+  });
+});
