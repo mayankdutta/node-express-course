9e833a53dcdefd95af666f1a56201f1e5b86afaf
Mayank Sharma <info@Mayanks-MacBook-Air.local>
2021-04-21 20:16:30 +0530

    added login post method

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.json());
 const mockUserData = [{ name: "Mayank" }, { name: "Dutta" }];
+
+app.pos("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const name = "mayank";
+  const pass = "12345";
+
+  if (username === name && pass == password) {
+    res.json({
+      success: true,
+      message: "password and name matched",
+      token: "encrypted token goes here",
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "chalaja yaha se",
+    });
+  }
+});
+
 app.get("/users", function (req, res) {
   res.json({
     success: true,
@@ -9,6 +33,7 @@ app.get("/users", function (req, res) {
     users: mockUserData,
   });
 });
+
 app.get("/users/:id", function (req, res) {
   console.log(req.params.id);
   res.json({
